220fabaa23d842e7a1b8e1cf5012a2f47e5bb2fb
Roshkanyuk.Roman <roshkanyuk.roman@ssakhk.cz>
2025-08-29 22:22:01 +0200

    ASDQWS

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -132,7 +132,7 @@ const Filter = (props: { product: ItemProps[],  separated: ItemProps[][], setSep
   }
   return (
     
-    <div className="bg-[#1D1D1DF7] bg-opacity-80 text-opacity-80 p-7 w-[350px] ml-3  text-white absolute mt-4 z-10 max-h-[300px] overflow-y-auto">
+    <div className={`bg-[#1D1D1DF7] bg-opacity-80 text-opacity-80 p-7 w-[350px] ml-3  text-white absolute mt-4 z-10 max-h-[${window.innerHeight< 500 ? 300 : 500}px] overflow-y-auto`}>
       <p className="text-[18px] font-bold">Filtry:</p>
       <div className=" flex justify-between">
         <p className="text-[18px] mt-[10px]">Seřadit podle:</p>
