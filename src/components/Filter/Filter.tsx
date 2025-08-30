fd26db3f892acef16a59f568a454c6f9b88225f0
Roshkanyuk.Roman <roshkanyuk.roman@ssakhk.cz>
2025-08-31 00:37:29 +0200

    qwert

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -138,7 +138,7 @@ const Filter = (props: { product: ItemProps[],  separated: ItemProps[][], setSep
   }
   return (
     
-    <div className={`bg-[#1D1D1DF7] bg-opacity-80 text-opacity-80 p-7 w-[350px] ml-3  text-white absolute mt-4 z-10 max-h-[${width < 500 ? 300 : 500}px] overflow-y-auto`}>
+    <div className={`bg-[#1D1D1DF7] bg-opacity-80 text-opacity-80 p-7 w-[350px] ml-3  text-white absolute mt-4 z-10 max-h-[${width > 500 ? 300 : 500}px] overflow-y-auto`}>
       <p className="text-[18px] font-bold">Filtry:</p>
       <div className=" flex justify-between">
         <p className="text-[18px] mt-[10px]">Seřadit podle:</p>
