476a85b9ee4b4d3aaac85cf8564ddf522f317317
rizzer18 <romanroskanuk74@gmail.com>
2025-12-09 12:51:11 +0100

    qwe

--- src/apis_reqests/products.ts
+++ src/apis_reqests/products.ts
@@ -118,7 +118,7 @@ export async function delete_product(id: number) {
 
 export async function make_order(order: Order): Promise<string | null> {
   return await reqest
-    .post(`/api/orders`, order, { headers: {}})
+    .post(`/api/orders`, order)
     .then((e) => { alert(e.status); return "ok"})
     .catch((e) => {
       alert(e.response.data);
