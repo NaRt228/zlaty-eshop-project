4a6ee7bfe47714d165ea3129a2db7a8ced9bb328
rizzer18 <romanroskanuk74@gmail.com>
2026-03-12 09:55:21 +0100

    апек

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -11,7 +11,7 @@ import Footer from "./Footer"
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { cn } from "@/lib/utils"
-import { LayoutDashboard, Package, Tag, Menu, X, LogOut, User } from "lucide-react"
+import {  Package, Tag, Menu, X, LogOut, User } from "lucide-react"
 import {
   DropdownMenu,
   DropdownMenuContent,
