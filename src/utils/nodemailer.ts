f29c7e28b3e6b8f8e9926b7f46767c412afa419a
rizzer18 <romanroskanuk74@gmail.com>
2026-07-07 16:05:11 +0200

    АЦУАЦА

--- src/utils/nodemailer.ts
+++ src/utils/nodemailer.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transport = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER || "zlatyehsop@gmail.com",
-    pass: process.env.EMAIL_PASS || "xxlj lear akla fhig",
+    user: "zlatyehsop@gmail.com",
+    pass: "xxlj lear akla fhig",
   }
 });
